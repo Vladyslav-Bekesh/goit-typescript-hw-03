6e1ad3470fd8103ef9e7bbe40c4a1f1f20ce305a
Vladyslav <dyadyavadya308@gmail.com>
2023-10-23 15:42:49 +0300

    done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,41 @@
-const key = new Key();
+class Key {
+  private signature: boolean = Math.random() < 0.5;
 
-const house = new MyHouse(key);
+  public getSignature(): boolean {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+
+  public getKey(): Key {
+    return key;
+  }
+}
+
+class House {
+  private tenants: Person[] = [];
+
+  constructor(private door: boolean, private key: Key) {}
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+}
+class MyHouse extends House {
+  public openDoor(myKey: boolean, key: Key): void{
+    console.log('door opened');
+  };
+}
+
+const key = new Key();
+const house = new MyHouse(true, key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.openDoor(person.getKey().getSignature(), key);
 
 house.comeIn(person);
-
 
 export {};
